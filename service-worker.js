ec74e65c941fe49766c42000ef108541699f1453
CONNECTMolly <connect.m.c.o@gmail.com>
2026-07-11 23:15:48 +0900

    Update offline cache for deployed app files

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,34 @@
-const CACHE="wadaiko-matsuri-v11";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
-self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const y=x.clone();caches.open(CACHE).then(c=>c.put(e.request,y));return x}).catch(()=>caches.match("./index.html"))))});
+const CACHE="wadaiko-matsuri-v12";
+const ASSETS=[
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./app-1.js",
+  "./app-2.js",
+  "./app-3.js",
+  "./app-4.js",
+  "./app-5.js",
+  "./app-6.js",
+  "./manifest.webmanifest",
+  "./icon.svg",
+  "./まつり2026-07-11.json"
+];
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener("activate",event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET")return;
+  event.respondWith(
+    caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+      return response;
+    }).catch(()=>caches.match("./index.html")))
+  );
+});
